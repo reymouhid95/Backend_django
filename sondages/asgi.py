a9716b853826bd3d663264670b1643d40ea782f3
reymouhid95 <thiernooury89@gmail.com>
2024-02-11 15:34:45 +0000

    authentification terminée

--- sondages/asgi.py
+++ sondages/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for pulso project.
+ASGI config for sondages project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pulso.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sondages.settings')
 
 application = get_asgi_application()
