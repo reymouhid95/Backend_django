429b7f2f083f3a0aad63968110ebe2c06deb0b0c
MeedBechir <medbechirathie@gmail.com>
2024-02-12 17:29:10 +0000

    Testing the deploymenent

--- sondages/settings.py
+++ sondages/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+
 from datetime import timedelta
+
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -80,16 +83,20 @@ WSGI_APPLICATION = 'sondages.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'surver-back',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Moh@med9394@',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     },
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'surver-back',
-        'USER': 'postgres',
-        'PASSWORD': 'Moh@med9394@',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    },
-}
+    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+} 
 
 
 REST_FRAMEWORK = {
@@ -134,6 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- sondages/urls.py
+++ sondages/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/user/', include('account.urls')),
     path('api/', include('account.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
