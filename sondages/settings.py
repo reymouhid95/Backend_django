7ef5ee5e324dea5b86e9244e8e24c450e6d87105
MeedBechir <medbechirathie@gmail.com>
2024-02-19 15:07:06 +0000

    Added a link to the the cors allowed origins

--- sondages/settings.py
+++ sondages/settings.py
@@ -174,4 +174,5 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:5173",
     "https://survey-project-rho.vercel.app",
+    "https://testing-pulso-deployment.vercel.app/",
 ]
